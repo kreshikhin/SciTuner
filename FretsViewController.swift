6e100371f05b58ddbf225255b5d9408d30eaf913
Denis Kreshikhin <denis@kreshikhin.com>
2015-03-08 23:53:29 +0300

    fixed #15 added frets

---
+++ FretsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FretsViewController.swift
+//  SciTuner
+//
+//  Created by Denis Kreshikhin on 08.03.15.
+//  Copyright (c) 2015 Denis Kreshikhin. All rights reserved.
+//
+
+import Foundation
