18755495e968d3edf6265b10f8db369a1ebf66c0
Denis Kreshikhin <denis@kreshikhin.com>
2017-08-31 16:39:44 +0300

    Add FilterTests

---
+++ SciTunerTests/Models/FilterTests.swift
@@ -0,0 +1,15 @@
+//
+//  FilterTests.swift
+//  SciTuner
+//
+//  Created by Denis Kreshikhin on 8/31/17.
+//  Copyright © 2017 Denis Kreshikhin. All rights reserved.
+//
+
+import XCTest
+
+class FilterTests: XCTestCase {
+    func testFilterStates() {
+        XCTAssertNotEqual(Filter.allFilters.count, 0)
+    }
+}
